b50054ec66ebe33df7d2f38a7e9004278a37c164
DucTT <mrtranduc1994@outlook.com>
2023-08-14 00:40:59 +0700

    style homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
 
 export default function Home() {
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <h1>hello babe</h1>
+        <main className="text-center">
+            <div>
+                <h1 className="text-2xl mt-16 mb-8">Welcome to my World</h1>
+                <ul>
+                    <li>Go to <Link href='/calendar'>
+                        <span className="hover:underline hover:text-sky-400"> Google Calendar</span>
+                    </Link></li>
+                </ul>
             </div>
         </main>
     )
